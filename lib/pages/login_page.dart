b4703991ccb3a0ffe6f7deb496334fdd03fed164
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2021-06-02 01:41:05 +0530

    day 5 & 6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,16 @@
+import 'dart:ui';
+
 import "package:flutter/material.dart";
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,9 +26,9 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
-                fontSize: 30,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
@@ -33,22 +42,68 @@ class LoginPage extends StatelessWidget {
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
                   ),
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                      child: Text("Login"),
-                      onPressed: () {
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40))),
+
+                  InkWell(
+                    splashColor: Colors.purpleAccent,
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(150, 40),
+                  //   ),
+                  // ),
                 ],
               ),
             ),
