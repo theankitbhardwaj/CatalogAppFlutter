8d5fb54e394c6d73cca49063f8af752e39b4bb12
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2021-06-01 19:15:32 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Container(
+          child: Center(
+            child: Text("Welcome to app."),
+          ),
+        ),
+      ),
+    );
+  }
+}
