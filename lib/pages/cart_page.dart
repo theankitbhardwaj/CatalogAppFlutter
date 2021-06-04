e47661b17561151de06a5811b74745cb63b22863
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2021-06-04 19:40:38 +0530

    day 26

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/models/cart.dart';
-import 'package:flutter_catalog/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -62,14 +61,7 @@ class _CartTotal extends StatelessWidget {
   }
 }
 
-class _CartList extends StatefulWidget {
-  const _CartList({Key? key}) : super(key: key);
-
-  @override
-  __CartListState createState() => __CartListState();
-}
-
-class __CartListState extends State<_CartList> {
+class _CartList extends StatelessWidget {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
@@ -80,7 +72,9 @@ class __CartListState extends State<_CartList> {
             itemBuilder: (context, index) => ListTile(
               leading: Icon(Icons.done),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  _cart.remove(_cart.items[index]);
+                },
                 icon: Icon(CupertinoIcons.delete),
               ),
               title: _cart.items[index].name.text.make(),
