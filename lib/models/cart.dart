868f3bf78176a73fb35928e98d1a357ff4b10e93
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2021-06-04 18:33:08 +0530

    day 23

---
+++ lib/models/cart.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  late CatalogModel _catalog;
+
+  final List _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  num get totalPrice =>
+      items.fold(0, (total, currentItem) => total + currentItem.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
